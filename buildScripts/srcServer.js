395ac302e52b5d777906cc33f831cd80f1ad51e8
Kasper Juel <kaped82@gmail.com>
2016-11-15 12:09:27 +0100

    Setting up for HTTP Calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,14 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1, "firstName": "Gene", "lastName": "Belcher", "email": "gene@themachine.com"},
+    {"id": 2, "firstName": "Tina", "lastName": "Belcher", "email": "tina@horselovers.com"},
+    {"id": 3, "firstName": "Bob", "lastName": "Belcher", "email": "bobs@burger.com"}
+  ]);
+});
+
 app.listen(port, function(err){
     if (err) {
         console.log(err);
